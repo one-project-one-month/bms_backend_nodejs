a8390e470e9e791d3f8932d6a11091160dad6f48
mr-kmh <kgminhtet.dev@gmail.com>
2024-05-18 21:46:57 +0630

    database connected

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import usersRoutes from './users/users.routes.js';
+import db, {connectDatabase} from "./database/index.js"
 
+connectDatabase();
 const app = express();
 
 app.use(express.json()); // Enable JSON parsing for all routes
 app.use(cors()); // Enable CORS for all routes
 
-app.use('/users', usersRoutes);
-app.use('/', (req, res) => {
-  res.send('Bank Management System API!');
-});
 
 export default app;
-
--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function disconnect() {
+export function connectDatabase() {
+    prisma.$connect().then(() => {
+      console.log('Connected to database');
+    }).catch((err) => {
+      console.error('Error connecting to database', err);
+    });
+}
+
+export async function disconnectDabase() {
   await prisma.$disconnect();
 }
 
