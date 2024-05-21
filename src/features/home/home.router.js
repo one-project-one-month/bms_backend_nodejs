e3e6a8ba0afb307c470f29a20e62a34a2068008d
mr-kmh <kgminhtet.dev@gmail.com>
2024-05-21 20:36:10 +0630

    kind properties

--- src/features/home/home.router.js
+++ src/features/home/home.router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   return res.json({
     links: {
+      kind: "Bank Management System",
       self: `${HOST}:${PORT}`,
       admins: `${HOST}:${PORT}/api/${V1}/admins`,
     },
