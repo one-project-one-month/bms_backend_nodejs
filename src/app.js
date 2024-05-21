3b954bc970b2190f9d7f08939e0d9cfbccb0236e
mr-kmh <kgminhtet.dev@gmail.com>
2024-05-21 18:16:37 +0630

    fix

--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json()); // Enable JSON parsing for all routes
 app.use(cors()); // Enable CORS for all routes
 
-app.use("/api/v1/admins", adminRoute);
+app.use("/admins", adminRoute);
 
 export default app;
