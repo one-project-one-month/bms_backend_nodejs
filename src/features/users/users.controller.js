da04ebd10bb26c4f360e6442a3e3ad4c116695a2
mr-kmh <kgminhtet.dev@gmail.com>
2024-05-22 21:33:40 +0630

    refactor http status codes to meaningful names

--- src/features/users/users.controller.js
+++ src/features/users/users.controller.js
@@ -1,4 +1,4 @@
-import { matchedData, validationResult } from "express-validator";
+import httpStatus from "http-status-codes";
 import userServices from "./users.service.js";
 import { exceptionHandler } from "../../handlers/exception.js";
 import { apiRes } from "../../response/response.js";
@@ -13,17 +13,19 @@ const findAllUsers = async (req, res) => {
       switch (user.message) {
         case "NotFoundError":
           return res
-            .status(400)
+            .status(httpStatus.BAD_REQUEST)
             .json({ message: `not found user with email: ${email}` });
         default:
-          return res.status(500).json({ message: "something went wrong!!!!" });
+          return res
+            .status(httpStatus.INTERNAL_SERVER_ERROR)
+            .json({ message: "something went wrong!!!!" });
       }
     }
-    return res.status(200).json(resp.one(user));
+    return res.status(httpStatus.OK).json(resp.one(user));
   }
   let users = await userServices.findAll();
   users = users.map((user) => resp.one(user));
-  return res.status(200).json(resp.collection(users));
+  return res.status(httpStatus.OK).json(resp.collection(users));
 };
 
 const findUserById = async (req, res) => {
@@ -33,44 +35,46 @@ const findUserById = async (req, res) => {
     switch (data.message) {
       case "NotFoundError":
         return res
-          .status(400)
+          .status(httpStatus.BAD_REQUEST)
           .json({ message: `not found user with id ${id}` });
       default:
-        return res.status(500).json({ message: "something went wrong!!!!" });
+        return res
+          .status(httpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: "something went wrong!!!!" });
     }
   }
-  return res.status(200).json(data);
+  return res.status(httpStatus.OK).json(data);
 };
 
 const createUser = async (req, res) => {
-  const result = validationResult(req);
-  if (result.isEmpty()) {
-    const data = matchedData(req);
-    console.info("create user data ", data);
-    const newUser = await exceptionHandler(userServices.create)(req.body);
-    if (newUser instanceof Error) {
-      return res.status(500).json({ message: newUser.message });
-    }
-    return res.status(201).json(resp.one(newUser));
+  const newUser = await exceptionHandler(userServices.create)(req.body);
+  if (newUser instanceof Error) {
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: newUser.message });
   }
-  return res.status(400).json({ errors: result.array() });
+  return res.status(httpStatus.OK).json(resp.one(newUser));
 };
 
 const updateUser = async (req, res) => {
   const id = req.params["id"];
   const updatedUser = await exceptionHandler(userServices.update)(id, req.body);
   if (updateUser instanceof Error) {
-    return res.status(500).json({ message: updateUser.message });
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: updateUser.message });
   }
-  return res.status(200).json(resp.one(updatedUser));
+  return res.status(httpStatus.OK).json(resp.one(updatedUser));
 };
 
 const deactivateUser = async (req, res) => {
   const id = req.params["id"];
   const deactivatedUser = await exceptionHandler(userServices.deactivate)(id);
   if (deactivatedUser instanceof Error)
-    return res.status(500).json({ message: deactivatedUser.message });
-  return res.status(200).json(resp.one(deactivatedUser));
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: deactivatedUser.message });
+  return res.status(httpStatus.OK).json(resp.one(deactivatedUser));
 };
 
 const userActions = async (req, res) => {
@@ -79,7 +83,9 @@ const userActions = async (req, res) => {
     case "deactivate":
       return deactivateUser(req, res);
     default:
-      return res.status(405).json({ message: "Invalid action type" });
+      return res
+        .status(httpStatus.METHOD_NOT_ALLOWED)
+        .json({ message: "Invalid action type" });
   }
 };
 
@@ -87,8 +93,10 @@ const deleteUser = async (req, res) => {
   const id = req.params["id"];
   const deletedUser = await exceptionHandler(userServices.remove)(id);
   if (deletedUser instanceof Error)
-    return res.status(500).json({ message: deletedUser.message });
-  return res.status(200).json(resp.one(deletedUser));
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: deletedUser.message });
+  return res.status(httpStatus.ACCEPTED).end();
 };
 
 export default {
