f58a18894b8fcc7dbb4a7c8bef8b13f6d9b3a035
mr-kmh <kgminhtet.dev@gmail.com>
2024-05-24 17:00:16 +0630

    replace V1 with api/v1

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import { connectDatabase } from "./database/index.js";
-import { V1 } from "./configs.js";
 import homeRoute from "./features/home/home.router.js";
 import adminRoute from "./features/admins/admins.routes.js";
 import userRoute from "./features/users/users.routes.js";
@@ -18,8 +17,8 @@ app.use(express.json()); // Enable JSON parsing for all routes
 app.use(cors()); // Enable CORS for all routes
 
 app.use("/", homeRoute);
-app.use(`/${V1}/admins`, adminRoute);
-app.use(`/${V1}/users`, userRoute);
-app.use(`/${V1}/transactions`, transactionRoute);
+app.use(`/api/v1/admins`, adminRoute);
+app.use(`/api/v1/users`, userRoute);
+app.use(`/api/v1/transactions`, transactionRoute);
 
 export default app;
